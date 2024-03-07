5c68d6b4a82803ba1e5c5796ba5aa790053cf3dc
rishab1444 <rishab1444@gmail.com>
2024-03-07 19:37:15 +0530

    Project intialization

---
+++ Bitbackend-task/src/main/java/com/example/Bitbackendtask/BitbackendTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bitbackendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbackendTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbackendTaskApplication.class, args);
+	}
+
+}
---
+++ Bitbackend-task/src/test/java/com/example/Bitbackendtask/BitbackendTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bitbackendtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbackendTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
